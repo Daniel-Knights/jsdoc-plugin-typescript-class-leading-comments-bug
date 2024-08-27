3b98b9e378b6f5cde2a1adb9c416173b0c66e0ec
Daniel Knights <59598622+Daniel-Knights@users.noreply.github.com>
2024-08-27 10:41:17 +0100

    init

---
+++ a.js
@@ -0,0 +1,15 @@
+import "";
+
+/*
+  Comment
+*/
+
+/**
+ * Doclet
+ */
+export class A {}
+
+// Import statement required for JSDoc to include first comment as leading comment of the class.
+// Otherwise, it gets moved in the AST to the `Program` node from the `ExportNamedDeclaration` class node:
+// https://github.com/jsdoc/jsdoc/blob/main/packages/jsdoc-ast/lib/walker.js#L465
+// This itself may be a bug, as it seems to be intended for @module comments.
